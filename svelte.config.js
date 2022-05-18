4f5026f1de9ad83cbc59eaa7a2af7bfac853e6db
Brittney Postma <brittneypostma@Brittneys-MacBook-Pro.local>
2022-05-18 12:45:12 -0400

    removed split

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,7 @@ import adapter from '@sveltejs/adapter-netlify'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      split: true
-    })
+    adapter: adapter()
   }
 }
 
