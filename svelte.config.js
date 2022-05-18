fbc2a7f1d4577e273831c3ee412f741a800805af
Brittney Postma <brittneypostma@Brittneys-MacBook-Pro.local>
2022-05-18 11:48:32 -0400

    added split to config

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
-};
+  kit: {
+    adapter: adapter({
+      split: true
+    })
+  }
+}
 
-export default config;
+export default config
