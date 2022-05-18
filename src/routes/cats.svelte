9df760cea4b9f863811d8085d25a783672241892
Brittney Postma <brittneypostma@Brittneys-MacBook-Pro.local>
2022-05-18 12:17:16 -0400

    prerendered routes

--- src/routes/cats.svelte
+++ src/routes/cats.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export let prerender = true
+</script>
+
 <script>
 	export let cats
 </script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export let prerender = true
+</script>
+
 <h1>CatPhotoApp</h1>
 <main>
 	<section>
